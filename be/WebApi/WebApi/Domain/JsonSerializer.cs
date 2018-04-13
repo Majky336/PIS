51506020b40e6283a23acb4180c548417e5c6e17
Kvas1r <09krajcovic@gmail.com>
2018-04-13 17:17:37 +0200

    Initional commit of BE part

---
+++ be/WebApi/WebApi/Domain/JsonSerializer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Script.Serialization;
+
+namespace WebApi.Domain
+{
+    public class JsonSerializer
+    {
+
+        public string GetJson(Object T)
+        {
+            var js = new JavaScriptSerializer();
+            string result = js.Serialize(T);
+            return result;
+        }
+    }
+}
