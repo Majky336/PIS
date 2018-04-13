94c87bd65177b3dc3ce33cf161a8db81a44c27f2
Kvas1r <09krajcovic@gmail.com>
2018-04-13 17:17:37 +0200

    Initional commit of BE part

---
+++ be/WebApi/WebApi/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using WebApi.Domain;
+using WebApi.PouzivatelService;
+
+namespace WebApi.Controllers
+{
+    public class LoginController : ApiController
+    {
+        public virtual JsonSerializer GetJsonSerializer()
+        {
+            return new JsonSerializer();
+        }
+
+        // POST: api/Login
+        //POST: "/login" - Params: e-mail-string, heslo-string - Return: user-Pouzivatel || null 
+        [HttpPost]
+        public HttpResponseMessage Login(string email, string heslo)
+        {
+            var service = new Team024PouzivatelPortTypeClient();
+
+            var result = service.getByAttributeValue("email", email, new int?[] { });
+
+            var pouzivatel = result.FirstOrDefault(user => user.heslo == heslo);
+
+            var json = GetJsonSerializer().GetJson(pouzivatel);
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(json, Encoding.UTF8, "application/json");
+            return response;
+        }
+    }
+}
---
+++ be/WebApi/WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace WebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        //// GET api/values
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // GET api/values
+        public HttpResponseMessage GetJson()
+        {
+            var array = new [] { "value1", "value2" };
+            JavaScriptSerializer js = new JavaScriptSerializer(); //system.web.extension assembly....
+            string yourJson = js.Serialize(array);
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(yourJson, Encoding.UTF8, "application/json");
+            return response;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
