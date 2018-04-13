be90bfdb97e867973915f5b4ee481756c64f8c87
Majky336 <sojka.michal.96@gmail.com>
2018-04-13 17:29:37 +0200

    Added axios

--- fe/src/Pages/LoginPage.js
+++ fe/src/Pages/LoginPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 
+import { api } from '../api/api';
 import './LoginPage.css';
 
 class LoginPage extends Component {
@@ -46,6 +47,15 @@ class LoginPage extends Component {
     if (!password) {
       this.setState({ passwordError: 'Toto pole je povinné' })
     }
+
+    api.post('/api/login', {
+      eMail: 'konomrd',
+      heslo: 'konomrd',
+    }).then(response => {
+      console.log(response);
+    }).catch(error => {
+      console.log(error);
+    });
   }
 
   render() {
