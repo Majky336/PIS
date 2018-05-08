16fd4de94492311e8d4863a913ed0bd130bf7053
Kvas1r <09krajcovic@gmail.com>
2018-05-08 08:10:47 +0200

    added example projekt for simple service usage

---
+++ be/WebApi/UkazkaSluzieb/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UkazkaSluzieb.ServiceReference1;
+
+namespace UkazkaSluzieb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceClient = new Team024KnihaPortTypeClient();
+
+            var result = serviceClient.getAll();
+        }
+    }
+}
